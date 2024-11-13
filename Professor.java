e57d2e60ae1d72305e80565cf0955260f83fc516
Gustavo campos anisio <142316756+cgustavoa@users.noreply.github.com>
2024-11-13 17:06:30 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,5 +1,22 @@
 package br.com.grupo.trabalho;
 
-public class Professor {
+public class Professor extends Pessoa{
+    private String disciplina;
 
+    public Professor(String nome, int idade, String endereco, String disciplina) {
+        super(nome, idade, endereco);
+        this.disciplina = disciplina;
+    }
+
+    public void agendarAula(String horario) {
+        System.out.println(getNome() + " agendou uma aula de " + disciplina + " para " + horario);
+    }
+
+    public void cancelarAula(String horario) {
+        System.out.println(getNome() + " cancelou a aula de " + disciplina + " para " + horario);
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
 }
