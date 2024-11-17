f8f3b64935a9b65f4a4efbfad4b41a2f318141fe
Jefferson lucas <128716417+jeffersonLucass@users.noreply.github.com>
2024-11-17 20:35:39 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,22 +1,60 @@
 package br.com.grupo.trabalho;
 
-public class Professor extends Pessoa{
-    private String disciplina;
+import java.util.Scanner;
 
-    public Professor(String nome, int idade, String endereco, String disciplina) {
+import br.com.grupo.trabalho.fact.IniciarCurso;
+import br.com.grupo.trabalho.inter.Agendavel;
+
+public class Professor extends Pessoa implements Agendavel {
+	Scanner scanner = new Scanner(System.in);
+	
+    private String cursoLecionado;
+
+    public Professor(String nome, int idade, String endereco, String cursoLecionado) {
         super(nome, idade, endereco);
-        this.disciplina = disciplina;
+        this.cursoLecionado = cursoLecionado;
+    }
+    
+    public String getCursoLecionado() {
+		return cursoLecionado;
+	}
+
+	public void setCursoLecionado(String cursoLecionado) {
+		this.cursoLecionado = cursoLecionado;
+	}
+
+	@Override
+	public boolean agendarAula(IniciarCurso cursos) {
+		boolean validacao;
+		do {
+			System.out.print("\nInforme o dia da semana que deseja marcar a aula(Ex: Segunda-feira): ");
+			String diaAula = scanner.nextLine();
+			validacao = cursos.setDiaAula(diaAula);
+		} while(validacao == false);
+		do {
+			System.out.print("Informe o horário da aula(Ex: hh mm ss): ");
+			int hora = scanner.nextInt();
+	        int minuto = scanner.nextInt(); 
+	        int segundo = scanner.nextInt();
+	        scanner.nextLine();
+	        validacao = cursos.setHorarioAula(hora, minuto, segundo);
+		} while(validacao == false);
+		System.out.println("\nAula agendada com sucesso.");
+		return validacao;
     }
 
-    public void agendarAula(String horario) {
-        System.out.println(getNome() + " agendou uma aula de " + disciplina + " para " + horario);
+    @Override
+    public boolean cancelarAula(IniciarCurso cursos) {
+    	if(cursos.getDiaAula() == null) {
+			System.out.println("O(a) senhor(a) não tem nenhuma aula agendada que possa desmarcar.");
+			return false;
+		} else {
+			System.out.print("\nSua aula " + cursos.getDiaAula() + " às " + cursos.getHorarioAula() + "h.");
+			cursos.setDiaAula(null);
+			cursos.setHorarioAula(00, 00, 00);
+			System.out.println(" Foi cancelada com sucesso.");		
+			return true;
+		}
     }
 
-    public void cancelarAula(String horario) {
-        System.out.println(getNome() + " cancelou a aula de " + disciplina + " para " + horario);
-    }
-
-    public String getDisciplina() {
-        return disciplina;
-    }
 }
