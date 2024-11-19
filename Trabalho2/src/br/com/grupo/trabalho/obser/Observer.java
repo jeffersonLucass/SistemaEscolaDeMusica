21bb0c2824d26177bea560798276edadf4ad6e76
gusta <gustavogjs10@gmail.com>
2024-11-19 13:37:52 -0300

    Add Observer

---
+++ Trabalho2/src/br/com/grupo/trabalho/obser/Observer.java
@@ -0,0 +1,5 @@
+package br.com.grupo.trabalho.obser;
+
+public interface Observer {
+    void update(String mensagem);
+}
---
+++ Trabalho2/src/br/com/grupo/trabalho/obser/Sujeito.java
@@ -0,0 +1,21 @@
+package br.com.grupo.trabalho.obser;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sujeito {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(String mensagem) {
+        for (Observer observer : observers) {
+            observer.update(mensagem);
+        }
+    }
+}
