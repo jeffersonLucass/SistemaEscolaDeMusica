3508a583a65b80e57ecafce15725315cca627d4a
Jefferson lucas <128716417+jeffersonLucass@users.noreply.github.com>
2024-11-20 16:56:28 -0300

    Delete Trabalho2 directory

--- Trabalho2/src/br/com/grupo/trabalho/inter/Agendavel.java
+++
@@ -1,8 +0,0 @@
-package br.com.grupo.trabalho.inter;
-
-import br.com.grupo.trabalho.fact.IniciarCurso;
-
-public interface Agendavel {
-	public boolean agendarAula(IniciarCurso cursos);
-	public boolean cancelarAula(IniciarCurso cursos);
-}
