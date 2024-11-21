3a3a2c831631e1bd1588538c0ff3071d777c690b
Jefferson lucas <128716417+jeffersonLucass@users.noreply.github.com>
2024-11-21 12:40:54 -0300

    Delete Teste/Trabalho2 directory

--- Teste/Trabalho2/src/br/com/grupo/trabalho/obser/Observer.java
+++
@@ -1,7 +0,0 @@
-package br.com.grupo.trabalho.obser;
-
-    //declaração da interface
-    public interface Observer {
-        //método abstrato que será implementado por todas as classes que quiserem ser observadores
-            void update(String mensagem);
-    }
